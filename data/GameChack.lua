f12b620861941b86344ab60a3981da212ff5e219
Hysonax <79469128+Hysonax@users.noreply.github.com>
2021-02-22 21:36:38 +0100

    Add files via upload

---
+++ data/GameChack.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 6304340354 then
+    GameName.Text = "ROWpuREd's Place"
+end
+if game.PlaceId == 6305942109 then
+    GameName.Text = "Laundry Simulator"
+end
+if game.PlaceId == 286090429 then
+    GameName.Text = "Arsenal"
+end

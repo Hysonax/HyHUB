cf847e8a665a5727682435d46b84298b83a7f706
Hysonax <79469128+Hysonax@users.noreply.github.com>
2021-02-23 12:06:39 +0100

    Update GameIDChack.lua

--- data/GameIDChack.lua
+++ data/GameIDChack.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Hysonax/HyHUB/main/data/Game%20Scripts/Arsenal/ArsenalMenu.lua",true))()
 elseif game.PlaceId == 6305942109 then
-    print("Not done yet!")
-else print("Unexpected error!!!")
+    warn("We working on!!!")
+else warn("Unknown game!!!")
 end
